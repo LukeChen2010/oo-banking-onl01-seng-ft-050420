1ed61265f0c655fdbbbf084e29044ed433bb5de8
Luke Chen <luke.chen.2010@gmail.com>
2020-05-10 23:09:14 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,24 @@
 class Transfer
-  # your code here
+  attr_accessor :sender, :receiver, :status, :amount
+  
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+    @amount = amount
+  end
+  
+  def valid?
+    return sender.valid? && receiver.valid?
+  end
+  
+  def execute_transaction
+    return "Transaction rejected. Please check your account balance." if sender.balance < @amount
+    return nil if @status == "complete"
+    
+    sender.deposit(-@amount)
+    receiver.deposit(@amount)
+    @status = "complete"
+    
+  end
 end
