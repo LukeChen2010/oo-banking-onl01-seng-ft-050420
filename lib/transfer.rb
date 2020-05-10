2f661480704dd35c311cec12d4ae5bb274597381
Luke Chen <luke.chen.2010@gmail.com>
2020-05-10 23:14:08 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,8 +13,8 @@ class Transfer
   end
   
   def execute_transaction
-    return "rejected" if !@sender.valid? || !@receiver.valid?
     return "Transaction rejected. Please check your account balance." if @sender.balance < @amount 
+    return "rejected" if !@sender.valid? || !@receiver.valid?
     return nil if @status == "complete"
     
     @sender.deposit(-@amount)
